919c3051773e46bd4bc4e0f70ed31ef8698e1241
rdpolarity <rdpolarity@gmail.com>
2019-08-01 23:05:26 +1000

    Updated

--- pages/components/footer.jsx
+++ pages/components/footer.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import Typography from "@material-ui/core";
+import { Typography, Grid } from "@material-ui/core";
 
-export default function footer() {
+export default function Footer() {
   return (
-    <div style={{ height: 200 }}>
-      <Typography>Copyright 2019 ©</Typography>
+    <div style={{ height: 100, color: "white" }}>
+      <Grid
+        container
+        justify="center"
+        alignContent="center"
+        style={{ height: "100%" }}
+      >
+        <Typography varient="p">Copyright 2019 ©</Typography>
+      </Grid>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import Document, { Head, Main } from "next/document";
 import Home from "./Home";
 import "../styles/index.scss";
+import Footer from "./components/Footer";
 
 export default function Index() {
   return (
     <html>
       <body>
         <Home />
+        <Footer />
       </body>
     </html>
   );
